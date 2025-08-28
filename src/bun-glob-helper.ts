45b7d0c8661ef443c444bab72a4b5d8165dd14fa
Kevin Frei <kevinfrei@hotmail.com>
2025-08-27 19:21:03 -0700

    Globbing input to make it usable

---
+++ src/bun-glob-helper.ts
@@ -0,0 +1,24 @@
+import { Glob } from 'bun';
+
+export function deGlob(pathOrGlob: string): Glob | undefined {
+  if (pathOrGlob.indexOf('*') >= 0 || pathOrGlob.indexOf('?') >= 0) {
+    let res = pathOrGlob;
+    res = res.replaceAll('\\', '\\\\');
+    if (res.startsWith('!')) res = `\\${res}`;
+    res = res.replaceAll('{', '\\{');
+    res = res.replaceAll('}', '\\}');
+    res = res.replaceAll('[', '\\]');
+    res = res.replaceAll(']', '\\]');
+    return new Glob(res);
+  }
+  return undefined;
+}
+
+export function expando(pathsOrGlobs: string[]): string[] {
+  return pathsOrGlobs
+    .map((v) => {
+      const theGlob = deGlob(v);
+      return theGlob ? [...theGlob.scanSync('.')] : v;
+    })
+    .flat();
+}
