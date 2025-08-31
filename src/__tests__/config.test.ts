c693b7496a1eada113feaba924998025de02c6df
Kevin Frei <kevinfrei@hotmail.com>
2025-08-30 17:23:15 -0700

    Updated tests to validate a bit more stuff

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -82,7 +82,7 @@ describe('config tests', () => {
       const config = loadConfig();
       // A couple control keywords, and an option
       const cmakeContent = `add_executable(myApp PuBlIc value value2
-      private value3 thingy value 5 value6 )`;
+      private value3 value 5 thingy value6 )`;
       const res = printString(cmakeContent, config);
       // console.log(res);
       expect(res).toBeDefined();
@@ -97,12 +97,55 @@ describe('config tests', () => {
       expect(lines[2]!).toBe('\tPUBLIC');
       expect(lines[3]!).toBe('\t\tvalue value2');
       expect(lines[4]!).toBe('\tPRIVATE');
-      expect(lines[5]!).toBe('\t\tvalue3');
-      expect(lines[6]!).toBe('\t\THINGY');
-      expect(lines[7]!).toBe('\tvalue');
-      expect(lines[8]!).toBe('\t5');
-      expect(lines[9]!).toBe('\tvalue6');
-      expect(lines[10]!).toBe(')');
+      expect(lines[5]!).toBe('\t\tvalue3 value 5');
+      expect(lines[6]!).toBe('\tTHINGY');
+      expect(lines[7]!).toBe('\tvalue6');
+      expect(lines[8]!).toBe(')');
+    } finally {
+      process.chdir(cwd);
+    }
+  });
+  test('config formatting check: more controlKeywords & options', () => {
+    const cwd = process.cwd();
+    try {
+      process.chdir(
+        path.dirname(getTestFileName('good-cfg-dir/test-dir/.passablerc.json')),
+      );
+      const config = loadConfig();
+      // A couple control keywords, and an option
+      const cmakeContent = `add_executable(myApp private value3 value 5 thingy value6
+      PuBlIc value value2
+      value4 value5.cpp value6.cpp value7.cpp value8.cpp value9.cpp value10.cpp
+      INHERIT value11 value12 )`;
+      const res = printString(cmakeContent, config);
+      // console.log(res);
+      expect(res).toBeDefined();
+      expect(res.indexOf('\r')).toBe(-1);
+      // There should be no blank lines in our output
+      const lines = res.split('\n');
+      const blank = lines.findIndex((line) => line.trim().length === 0);
+      expect(blank).toBe(-1);
+      expect(lines[0]!).toBe('add_executable(');
+      expect(lines[1]!).toBe('\tmyApp');
+      // From the config, indent args after the first by 1 more level for 'add_executable'
+      expect(lines[2]!).toBe('\tPRIVATE');
+      expect(lines[3]!).toBe('\t\tvalue3 value 5');
+      expect(lines[4]!).toBe('\tTHINGY');
+      expect(lines[5]!).toBe('\tvalue6');
+      expect(lines[6]!).toBe('\tPUBLIC');
+      expect(lines[7]!).toBe('\t\tvalue');
+      expect(lines[8]!).toBe('\t\tvalue2');
+      expect(lines[9]!).toBe('\t\tvalue4');
+      expect(lines[10]!).toBe('\t\tvalue5.cpp');
+      expect(lines[11]!).toBe('\t\tvalue6.cpp');
+      expect(lines[12]!).toBe('\t\tvalue7.cpp');
+      expect(lines[13]!).toBe('\t\tvalue8.cpp');
+      expect(lines[14]!).toBe('\t\tvalue9.cpp');
+      expect(lines[15]!).toBe('\t\tvalue10.cpp');
+      expect(lines[16]!).toBe('\tINHERIT');
+      expect(lines[17]!).toBe('\tvalue11');
+      expect(lines[18]!).toBe('\tvalue12');
+      expect(lines[19]!).toBe(')');
     } finally {
       process.chdir(cwd);
     }
